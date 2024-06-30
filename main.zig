65f40868034acbb24d8ca3101a1b1302d433fdf5
2814109 <2814109k@gmail.com>
2024-07-01 01:12:33 +0900

    add test

--- main.zig
+++ main.zig
@@ -2,9 +2,4 @@ const std = @import("std");
 
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
-    sample();
-}
-
-pub fn sample() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
 }
